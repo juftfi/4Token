b2874d196a7401837a5c5b9448bda8fddf05d1a4
juftfi <107974770+juftfi@users.noreply.github.com>
2026-06-21 07:34:49 +0700

    Update TreasuryAddress.tsx

--- src/components/TreasuryAddress.tsx
+++ src/components/TreasuryAddress.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const TREASURY_ADDRESS = "0x36f02e77f0868ad5446c08183280a542dbacffff";
+const TREASURY_ADDRESS = "4444";
 
 export function TreasuryAddress() {
   const [copied, setCopied] = useState(false);
